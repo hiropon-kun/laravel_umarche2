8b52619588d9e943567d4ce71928dc7d71bb899b
hiropon-kun <gudorobetta@yahoo.co.jp>
2022-04-03 10:59:28 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{$classBaseMessage}}</div>
+    <div>{{$defaultMessage}}</div>
+    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+</div>
