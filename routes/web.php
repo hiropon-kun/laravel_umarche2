5bc100b787dd4f2d7f91e98d90571ef54772728f
hiropon-kun <gudorobetta@yahoo.co.jp>
2022-04-26 10:14:51 +0900

    cart add

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\ItemController;
+use App\Http\Controllers\User\CartController;
 use App\Http\Controllers\ComponentTestController;
 
 
@@ -25,6 +26,12 @@ Route::middleware('auth:users')->group(function(){
         ->name('items.index');
         Route::get('show/{item}',[ItemController::class,'show'])
         ->name('items.show');
+    });
+
+Route::prefix('cart')
+    ->middleware('auth:users')->group(function(){
+        Route::post('add',[CartController::class,'add'])
+        ->name('cart.add');
     });
 
 // Route::get('/dashboard', function () {
